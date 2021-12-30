4ab359ab06401516b3938cdc3063e6ff1302fdcc
russell <r7wacc@gmail.com>
2021-12-30 13:59:58 +0700

    exercise 1.1

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import react from 'react'
+
+const Header = () => {
+    const course = 'Half Stack application development'
+    return (
+        <>
+            <div>
+                <p>{course}</p>
+            </div>
+        </>
+    )
+}
+
+export default Header
